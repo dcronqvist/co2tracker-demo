648cd7b1c4ce4f6557523348334bc1deff9d3bb0
Alexander Viala Bellander <alexander.viala.bellander@ivao.aero>
2020-12-01 15:57:45 +0100

    Add router and button

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
 import Submit from './components/Submit.js'
 import TreeView from './components/TreeView.js'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 const appStyle = {
   display: 'grid',
@@ -23,14 +30,56 @@ const title = {
   fontWeight: '300',
 }
 
+const button = {
+  borderRadius: '12px',
+  width: '8rem',
+  height: '2.5rem',
+  backgroundColor: 'white',
+  color: 'black',
+  display: 'grid',
+  placeItems: 'center',
+  boxShadow: '0px 4px 12px 0px rgba(0,0,0,0.20)',
+}
+
+const subHeader = {
+  display: 'grid',
+  margin: '25px',
+  justifyContent: 'center',
+  placeItems: 'center',
+  gridTemplateColumns: '150px 150px',
+}
+
 function App() {
   return (
     <div style={appStyle}>
       <header style={headerStyle}>
         <div style={title}>Co2 Tracker Demonstration</div>
       </header>
-      <Submit/>
-      <TreeView/>
+      <Router>
+        <div style={subHeader}>
+          <div style={button}>
+            <Link to="/submit" style={{ textDecoration: 'none' }}>
+            <div style={button}>
+              Submit
+            </div>
+            </Link>
+          </div>
+          <div style={button}>
+            <Link to="/supplychain" style={{ textDecoration: 'none' }}>
+            <div style={button}>
+              Supply Chain
+            </div>
+            </Link>
+          </div>
+        </div>
+
+        <Route path="/supplychain">
+          <TreeView/>
+        </Route>
+        <Route path="/submit">
+          <Submit/>
+        </Route>
+      </Router>
     </div>
   )
 }
